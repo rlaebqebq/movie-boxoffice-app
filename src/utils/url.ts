1a615b914b5482684fe344e83de092c2067889b6
rlaebqebq <rlaebqebq@gmail.com>
2022-06-05 02:07:37 +0900

    remove : 파일, 폴더 합침

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -3,9 +3,3 @@ export const getEnv = (): string => {
   if (l.hostname.includes('netlify')) return 'prod'
   return 'dev'
 }
-
-export const getGaKey = (): string => {
-  const env = getEnv()
-  if (env === 'prod') return process.env.REACT_APP_GA_KEY_PROD ?? ''
-  return process.env.REACT_APP_GA_KEY_DEV ?? ''
-}
