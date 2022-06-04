50a770cf35010a21e47e61cf6b0c614be39f5fe0
rlaebqebq <rlaebqebq@gmail.com>
2022-06-04 16:31:36 +0900

    style : scss 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
+
 import Boxoffice from './Main/Boxoffice'
 import styles from './Routes.module.scss'
 
 const App = () => {
   return (
     <div className={styles.appWrapper}>
-      <Routes>
-        <Route path='/' element={<Boxoffice />} />
-      </Routes>
+      <div className={styles.innerWrapper}>
+        <Routes>
+          <Route path='/' element={<Boxoffice />} />
+        </Routes>
+      </div>
     </div>
   )
 }
