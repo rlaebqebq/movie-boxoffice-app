72d77da57e8787a374fd06887c773474679f9178
rlaebqebq <rlaebqebq@gmail.com>
2022-06-10 07:10:01 +0900

    feat : movieQuery hook

---
+++ src/hooks/movieQuery.ts
@@ -0,0 +1,38 @@
+import { useQuery } from 'react-query'
+
+import { isAxiosError } from 'hooks/worker/axios'
+import { getBoxofficeApi, getMovieInfoApi } from 'utils/movie'
+import { IBoxofficeAPIRes } from 'types/dailyBoxoffice'
+import { IMovieSearchAPIRes } from 'types/movieInfo'
+
+export const useSearchDailyQuery = (targetDt: string) => {
+  return useQuery<IBoxofficeAPIRes, Error>(
+    ['getBoxofficeApi', targetDt],
+    () => getBoxofficeApi({ targetDt }).then((res) => res.data),
+    {
+      refetchOnWindowFocus: false,
+      onError(err) {
+        if (isAxiosError(err)) {
+          // eslint-disable-next-line no-console
+          console.log(err)
+        }
+      },
+    }
+  )
+}
+
+export const useSearchDetailQuery = (movieCd: string) => {
+  return useQuery<IMovieSearchAPIRes, Error>(
+    ['getMovieInfoApi', movieCd],
+    () => getMovieInfoApi({ movieCd }).then((res) => res.data),
+    {
+      refetchOnWindowFocus: false,
+      onError(err) {
+        if (isAxiosError(err)) {
+          // eslint-disable-next-line no-console
+          console.log(err)
+        }
+      },
+    }
+  )
+}
