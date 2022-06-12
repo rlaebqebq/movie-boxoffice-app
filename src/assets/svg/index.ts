4cfcf5888d7465b548efd2e154881b0fb5c33e05
rlaebqebq <rlaebqebq@gmail.com>
2022-06-12 20:47:11 +0900

    style : svg 추가

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,3 +1,8 @@
 export { ReactComponent as SpinnerIcon } from './spinner.svg'
+export { ReactComponent as MenuIcon } from './bars-solid.svg'
+export { ReactComponent as BackIcon } from './arrow-left-long.svg'
+export { ReactComponent as ArrowIcon } from './arrow.svg'
 export { ReactComponent as ArrowleftIcon } from './arrow-left.svg'
 export { ReactComponent as ArrowrightIcon } from './arrow-right.svg'
+export { ReactComponent as MovieTicketImg } from './movie-tickets.svg'
+export { ReactComponent as BookmarkIcon } from './bookmark.svg'
