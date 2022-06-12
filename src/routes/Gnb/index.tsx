209774f5bb02544e2da4f7617ee0a474c5cd5466
rlaebqebq <rlaebqebq@gmail.com>
2022-06-12 20:49:38 +0900

    feat : gnb 추가

---
+++ src/routes/Gnb/index.tsx
@@ -0,0 +1,21 @@
+import { BackIcon, MenuIcon } from 'assets/svg'
+import { NavLink, useLocation } from 'react-router-dom'
+import styles from './gnb.module.scss'
+
+const Gnb = () => {
+  const nowLocation = useLocation().pathname
+  return (
+    <>
+      {nowLocation !== '/' && (
+        <NavLink to='/' className={styles.backIcon}>
+          <BackIcon />
+        </NavLink>
+      )}
+      <NavLink to='mybookmark' className={styles.menuIcon}>
+        <MenuIcon />
+      </NavLink>
+    </>
+  )
+}
+
+export default Gnb
