a6b3a07b6eba9fc00ce5434e364721719ba509fd
rlaebqebq <rlaebqebq@gmail.com>
2022-06-13 04:09:54 +0900

    fix : https http

--- src/utils/movie.ts
+++ src/utils/movie.ts
@@ -5,7 +5,8 @@ import { IMoviePosterAPIRes } from 'types/moviePoster'
 
 const BASE_URL = 'https://kobis.or.kr/kobisopenapi/webservice/rest'
 
-const POSTER_BASE_URL = '//api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2'
+const POSTER_BASE_URL =
+  'http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2'
 
 interface IBoxofficeParams {
   targetDt: string
