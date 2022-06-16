0c31a91a14fec7de866b342c8df7821a0d39edc9
rlaebqebq <rlaebqebq@gmail.com>
2022-06-16 09:50:09 +0900

    feat : tmdb dictionary 추가

---
+++ src/routes/Movieinfo/MovieInfoPoster/genreDict.ts
@@ -0,0 +1,43 @@
+export interface IGenreDict {
+  28: number
+  12: number
+  16: number
+  35: number
+  80: number
+  99: number
+  18: number
+  10751: number
+  14: number
+  36: number
+  27: number
+  10402: number
+  9648: number
+  10749: number
+  878: number
+  10770: number
+  53: number
+  10752: number
+  37: number
+}
+
+export const genreDict = {
+  28: 'Action',
+  12: 'Adventure',
+  16: 'Animation',
+  35: 'Comedy',
+  80: 'Crime',
+  99: 'Documentary',
+  18: 'Drama',
+  10751: 'Family',
+  14: 'Fantasy',
+  36: 'History',
+  27: 'Horror',
+  10402: 'Music',
+  9648: 'Mystery',
+  10749: 'Romance',
+  878: 'ScienceFiction',
+  10770: 'TVMovie',
+  53: 'Thriller',
+  10752: 'War',
+  37: 'Western',
+}
--- src/routes/Movieinfo/MovieInfoPoster/movieinfoPosterTag.tsx
+++ src/routes/Movieinfo/MovieInfoPoster/movieinfoPosterTag.tsx
@@ -1,5 +1,6 @@
 import styles from '../movieinfo.module.scss'
 import cx from 'classnames'
+import { genreDict, IGenreDict } from './genreDict'
 
 interface Props {
   data?: number[]
@@ -7,34 +8,14 @@ interface Props {
 
 const MovieinfoPosterTag = ({ data }: Props) => {
   if (!data) return null
-  const genreList = {
-    '28': 'Action',
-    '12': 'Adventure',
-    '16': 'Animation',
-    '35': 'Comedy',
-    '80': 'Crime',
-    '99': 'Documentary',
-    '18': 'Drama',
-    '10751 ': 'Family',
-    '14': 'Fantasy',
-    '36': 'History',
-    '27': 'Horror',
-    '10402 ': 'Music',
-    '9648': 'Mystery',
-    '10749': 'Romance',
-    '878': 'ScienceFiction',
-    '10770': 'TVMovie',
-    '53': 'Thriller',
-    '10752': 'War',
-    '37': 'Western',
-  }
+
   return (
     <>
       {data.map((item, index) => {
         const key = `company-${item}-${index}`
         return (
           <p key={key} className={cx(styles.tagWrapper, styles.primaryTag)}>
-            {item}
+            {genreDict[item as keyof IGenreDict]}
           </p>
         )
       })}
