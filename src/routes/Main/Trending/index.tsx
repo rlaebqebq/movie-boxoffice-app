06fdcd3dd0941d52344692bcf27247b63efb2e80
rlaebqebq <rlaebqebq@gmail.com>
2022-06-26 14:46:37 +0900

    feat : trending bg

--- src/routes/Main/Trending/index.tsx
+++ src/routes/Main/Trending/index.tsx
@@ -1,18 +1,20 @@
 import { useTmdTrendingQuery } from 'hooks/tmdbQuery'
 import { genreDict, IGenreDict } from 'routes/MovieDetail/Movieinfo/genreDict'
 import styles from './trending.module.scss'
+import emptyPoster from 'assets/emptyPoster.png'
 
 const Trending = () => {
   const data = useTmdTrendingQuery('day').data?.results
   const checkHasImage = (sourceLink: string) => {
-    return `https://image.tmdb.org/t/p/w500${sourceLink}`
+    return sourceLink === null ? emptyPoster : `https://image.tmdb.org/t/p/w500${sourceLink}`
   }
+
   return (
     <ul className={styles.wrapper}>
       {data?.map((item, index) => {
         const key = `trending-${item}-${index}`
         return (
-          <li key={key} style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w500${item.backdrop_path})` }}>
+          <li key={key} style={{ backgroundImage: `url(${checkHasImage(item.backdrop_path)})` }}>
             <dl className={styles.rank}>
               <dt>{String(Math.floor(item.popularity))}</dt>
             </dl>
