e19a254b995a340b7a986e21957a87456cb29be9
rlaebqebq <rlaebqebq@gmail.com>
2022-06-26 12:56:03 +0900

    refactor : 파일 분할

--- src/routes/Boxoffice/boxofficeList.tsx
+++ src/routes/Boxoffice/boxofficeList.tsx
@@ -1,32 +1,24 @@
 import cx from 'classnames'
 import dayjs from 'dayjs'
-import { MouseEvent } from 'react'
 import { NavLink } from 'react-router-dom'
+import { MouseEvent } from 'react'
 
-import { ArrowleftIcon, ArrowrightIcon, BookmarkIcon } from 'assets/svg'
+import { BookmarkIcon } from 'assets/svg'
 import { useRecoilState } from 'hooks/state'
-import { todayDtState, targetMovieCdState, targetMovieOpenDtState, targetMovieNmState } from 'states/movie'
+import { targetMovieCdState, targetMovieOpenDtState, targetMovieNmState } from 'states/movie'
 import { IBoxOfficeResult } from 'types/dailyBoxoffice'
 import { isBookmarked } from 'utils/localStorage'
 
 import styles from './boxoffice.module.scss'
 
-interface Props {
+interface IProps {
   data?: IBoxOfficeResult
 }
 
-const BoxofficeList = ({ data }: Props) => {
-  const [todayDt, settodayDt] = useRecoilState(todayDtState)
+const BoxofficeList = ({ data }: IProps) => {
   const [, setTargetMovieCd] = useRecoilState(targetMovieCdState)
   const [, setTargetMovieOpenDt] = useRecoilState(targetMovieOpenDtState)
   const [, setTargetMovieNmState] = useRecoilState(targetMovieNmState)
-
-  const handlePrevWeek = () => {
-    settodayDt(todayDt.subtract(1, 'day'))
-  }
-  const handleNextWeek = () => {
-    if (dayjs().diff(todayDt, 'day') > 1) settodayDt(todayDt.add(1, 'day'))
-  }
 
   const handleMovieTarget = (e: MouseEvent<HTMLButtonElement>) => {
     const movieData = e.currentTarget.value.split('/')
@@ -36,49 +28,30 @@ const BoxofficeList = ({ data }: Props) => {
   }
 
   return (
-    <>
-      <div className={styles.title}>
-        <h2>BOXOFFICE</h2>
-        <div className={styles.innerWrapper}>
-          <button type='button' className={styles.prevButton} onClick={handlePrevWeek} aria-label='이전 날짜'>
-            <ArrowleftIcon />
-          </button>
-          <h3>{data?.showRange.substring(0, 8)}</h3>
-          <button type='button' className={styles.nextButton} onClick={handleNextWeek} aria-label='다음 날짜'>
-            <ArrowrightIcon />
-          </button>
-        </div>
-      </div>
-
-      <div className={styles.movieWrapper}>
-        <ul>
-          {data?.dailyBoxOfficeList.map((item) => (
-            <li key={`${item.movieNm}-${item.rank}`}>
-              <div className={cx(styles.bookmark, { [styles.bookmarkChecked]: isBookmarked(item.movieCd) })}>
-                <BookmarkIcon />
-              </div>
-              <NavLink to='/movieinfo'>
-                <button
-                  className={styles.innerWrapper}
-                  value={`${item.openDt}/${item.movieCd}/${item.movieNm}`}
-                  type='button'
-                  onClick={handleMovieTarget}
-                >
-                  <dl className={styles.rank}>
-                    <dt>{item.rank}</dt>
-                    <dd className={cx({ [styles.rankIntenPlus]: Number(item.rankInten) > 0 })}>{item.rankInten}</dd>
-                  </dl>
-                  <dl className={styles.info}>
-                    <dt>{item.movieNm}</dt>
-                    <dd>{item.openDt}</dd>
-                  </dl>
-                </button>
-              </NavLink>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
+    <ul className={styles.overflowWrapper}>
+      {data?.dailyBoxOfficeList.map((item) => (
+        <li key={`${item.movieNm}-${item.rank}`}>
+          <BookmarkIcon className={cx(styles.bookmark, { [styles.bookmarkChecked]: isBookmarked(item.movieCd) })} />
+          <NavLink to='/movieinfo'>
+            <button
+              className={styles.innerWrapper}
+              value={`${item.openDt}/${item.movieCd}/${item.movieNm}`}
+              type='button'
+              onClick={handleMovieTarget}
+            >
+              <dl className={styles.rank}>
+                <dt>{item.rank}</dt>
+                <dd className={cx({ [styles.rankIntenPlus]: Number(item.rankInten) > 0 })}>{item.rankInten}</dd>
+              </dl>
+              <dl className={styles.info}>
+                <dt>{item.movieNm}</dt>
+                <dd>{item.openDt}</dd>
+              </dl>
+            </button>
+          </NavLink>
+        </li>
+      ))}
+    </ul>
   )
 }
 
--- src/routes/Boxoffice/index.tsx
+++ src/routes/Boxoffice/index.tsx
@@ -1,21 +1,38 @@
 import { lazy, Suspense } from 'react'
 
-import { useRecoilValue } from 'hooks/state'
-import { useSearchDailyQuery } from 'hooks/movieQuery'
+import { useRecoilState } from 'hooks/state'
+import { useSearchDailyQuery } from 'hooks/kobisQuery'
 import { todayDtState } from 'states/movie'
 
+import { ArrowleftIcon, ArrowrightIcon, BoxofficeIcon } from 'assets/svg'
 import LoadingPage from 'components/LoadingPage'
 import styles from './boxoffice.module.scss'
+import dayjs from 'dayjs'
 
 const BoxofficeList = lazy(() => import('./boxofficeList'))
 
 const Boxoffice = () => {
-  const todayDt = useRecoilValue(todayDtState)
-  const formatTodayDt = todayDt.format('YYYYMMDD')
-  const data = useSearchDailyQuery(formatTodayDt).data?.boxOfficeResult
+  const [todayDt, settodayDt] = useRecoilState(todayDtState)
+  const data = useSearchDailyQuery(todayDt.format('YYYYMMDD')).data?.boxOfficeResult
+
+  const handlePrevWeek = () => {
+    settodayDt(todayDt.subtract(1, 'day'))
+  }
+  const handleNextWeek = () => {
+    if (dayjs().diff(todayDt, 'day') > 1) settodayDt(todayDt.add(1, 'day'))
+  }
 
   return (
-    <div className={styles.pageWrapper}>
+    <div className={styles.wrapper}>
+      <div className={styles.title}>
+        <button type='button' className={styles.prevButton} onClick={handlePrevWeek} aria-label='이전 날짜'>
+          <ArrowleftIcon />
+        </button>
+        <h3>Daily BoxOffice{data?.showRange.substring(0, 8)}</h3>
+        <button type='button' className={styles.nextButton} onClick={handleNextWeek} aria-label='다음 날짜'>
+          <ArrowrightIcon />
+        </button>
+      </div>
       <Suspense fallback={<LoadingPage />}>
         <BoxofficeList data={data} />
       </Suspense>
