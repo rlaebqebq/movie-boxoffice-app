3f56f32d6d2cf259d36662da8e5ace7af14757fe
rlaebqebq <rlaebqebq@gmail.com>
2022-07-01 15:25:05 +0900

    refactor : bookmark classname 변경

--- src/routes/MyBookmark/index.tsx
+++ src/routes/MyBookmark/index.tsx
@@ -3,27 +3,23 @@ import { useRecoilValue } from 'hooks/state'
 import { bookMarkList } from 'states'
 import { IBookmarkItem } from 'types'
 
-import BookmarkItem from './bookmarkItem'
-
 import styles from './myBookmark.module.scss'
 
 const MyBookmark = () => {
-  const list = useRecoilValue<IBookmarkItem[]>(bookMarkList)
+  const bookmarkList = useRecoilValue<IBookmarkItem[]>(bookMarkList)
 
   return (
     <div className={styles.wrapper}>
       <h2>My Favorites</h2>
-      <div className={styles.overflowWrapper}>
-        {list.length > 0 ? (
-          <ul className={styles.movieWrapper}>
-            {list.map((item) => (
-              <MovieCard key={item.movieCd} item={item} hasRank={false} backdropLink={item.backdropLink} />
-            ))}
-          </ul>
-        ) : (
-          <p className={styles.defaultMSG}>북마크가 비었습니다.</p>
-        )}
-      </div>
+      {bookmarkList.length > 0 ? (
+        <ul className={styles.overflowWrapper}>
+          {bookmarkList.map((item) => (
+            <MovieCard key={item.movieCd} item={item} hasRank={false} backdropLink={item.backdropLink} movieDetail />
+          ))}
+        </ul>
+      ) : (
+        <p className={styles.defaultMSG}>북마크가 비었습니다.</p>
+      )}
     </div>
   )
 }
