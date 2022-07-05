33ea3478924c36a0d0bc02de771b3b07edaaf2d1
rlaebqebq <rlaebqebq@gmail.com>
2022-07-05 23:43:31 +0900

    fix : loading

--- src/routes/SearchResult/index.tsx
+++ src/routes/SearchResult/index.tsx
@@ -1,15 +1,17 @@
-import { lazy, Suspense, useEffect } from 'react'
-
 import loadable from '@loadable/component'
 import { useInView } from 'react-intersection-observer'
 import { NavLink, useSearchParams } from 'react-router-dom'
+import { useEffect } from 'react'
 
-import styles from './searchResult.module.scss'
+import Loading from 'components/Loading'
 import { useSearchMovieQuery } from 'hooks/query'
-import { CloseIcon } from 'assets/svg'
 
-const Loading = loadable(() => import('components/Loading'))
-const MoviePages = lazy(() => import('./MoviePages'))
+import { CloseIcon } from 'assets/svg'
+import styles from './searchResult.module.scss'
+
+const MoviePages = loadable(() => import('./MoviePages'), {
+  fallback: <Loading />,
+})
 
 const SearchResult = () => {
   const { ref, inView } = useInView()
@@ -20,29 +22,29 @@ const SearchResult = () => {
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isFetching, isLoading } = useSearchMovieQuery(movieNm)
 
   const showFetchRef = hasNextPage && !isFetchingNextPage && data && !isFetching && !isLoading
+  const hasResult = !data || !movieNm || data.pages[0].totalCnt === 0
 
   useEffect(() => {
     if (inView) fetchNextPage()
   }, [fetchNextPage, inView])
 
   return (
-    <Suspense fallback={<Loading />}>
-      <div className={styles.wrapper}>
-        <NavLink to='/' className={styles.closeIcon}>
-          <CloseIcon />
-        </NavLink>
-        <h2>{movieNm}</h2>
-        <h3>Search Result : {data?.pages[0].totalCnt}</h3>
-        {!data || !movieNm || data.pages[0].totalCnt === 0 ? (
-          <p className={styles.defaultMSG}>검색 결과가 없습니다.</p>
-        ) : (
-          <div className={styles.overflowWrapper}>
-            <MoviePages data={data} />
-            {showFetchRef && <div ref={ref} />}
-          </div>
-        )}
-      </div>
-    </Suspense>
+    <div className={styles.wrapper}>
+      {/* {(isFetching || isLoading) && <Loading />} */}
+      <NavLink to='/' className={styles.closeIcon}>
+        <CloseIcon />
+      </NavLink>
+      <h2>{movieNm}</h2>
+      <h3>Search Result : {data?.pages[0].totalCnt}</h3>
+      {!hasResult ? (
+        <div className={styles.overflowWrapper}>
+          <MoviePages data={data} />
+          {showFetchRef && <div ref={ref} />}
+        </div>
+      ) : (
+        <p className={styles.defaultMSG}>검색 결과가 없습니다.</p>
+      )}
+    </div>
   )
 }
 
